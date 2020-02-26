084a1e6cdbb7e9ed004774c217001ff25a36a061
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    galen: Build and enable fastbootd
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab

--- device.mk
+++ device.mk
@@ -92,6 +92,10 @@ PRODUCT_PACKAGES += \
     nvaudio_fx.xml
 endif
 
+# fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Kernel
 ifneq ($(TARGET_PREBUILT_KERNEL),)
 TARGET_FORCE_PREBUILT_KERNEL := true
--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,10 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.media.avsync=true
 
+# fastbootd
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fastbootd.available=true
+
 # USB configfs
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.sys.usb.udc=3550000.xudc \
