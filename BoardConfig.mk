LineageOS/android_device_nvidia_foster

Add twrp support
WIP: TODO: Review flags and fix fstabs for new kernel
Change-Id: I099269e75b3b9cf72659e5a0ce893715c048e67b

BoardConfig.mk
@@ -57,6 +57,11 @@ BOARD_SUPPRESS_EMMC_WIPE := true
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/nvidia/foster/releasetools
 
+# TWRP Support
+ifeq ($(WITH_TWRP),true)
+include device/nvidia/foster/twrp/twrp.mk
+endif
+
 # Vendor Init
 TARGET_INIT_VENDOR_LIB := libinit_tegra libinit_shield libinit_foster
 

LineageOS/android_device_nvidia_galen

Change-Id: Ida26ab65876a1d93842875d98cd655c50d3c80fe

BoardConfig.mk
@@ -50,5 +50,10 @@ TARGET_RECOVERY_FSTAB := device/nvidia/galen/initfiles/fstab.jetson-xavier
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2019-07-05
 
+# TWRP Support
+ifeq ($(WITH_TWRP),true)
+include device/nvidia/galen/twrp/twrp.mk
+endif
+
 include device/nvidia/t194-common/BoardConfigCommon.mk
 include vendor/nvidia/galen/BoardConfigVendor.mk