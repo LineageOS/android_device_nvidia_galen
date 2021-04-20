2fcdd34580b36025d35716187fb3008bfda7f9a1
Aaron Kling <webgeek1234@gmail.com>
2021-04-20 12:03:50 -0500

    Install bootloader updates after ota update
    Also add support for generating initial flash packages
    
    Change-Id: I5e5517bca006c174c40a9fafca5183a929111ded

--- device.mk
+++ device.mk
@@ -134,6 +134,10 @@ PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
 # Updater
+PRODUCT_PACKAGES += \
+    nv_bootloader_payload_updater \
+    bl_update_payload \
+    bmp_update_payload
 AB_OTA_PARTITIONS += \
     boot \
     system \
@@ -142,5 +146,5 @@ AB_OTA_PARTITIONS += \
 AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    POSTINSTALL_PATH_system=system/bin/nv_bootloader_payload_updater \
     RUN_POSTINSTALL_system=true
