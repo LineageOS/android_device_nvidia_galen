db7a3dcfc4bb7dc92f146fb4d7915496893956cf
Aaron Kling <webgeek1234@gmail.com>
2021-10-11 17:35:44 -0500

    flash_packages: Simplify and refactor flash scripts
    Change-Id: Ib0ecfa8c618573765ab64400766b92ac951aa681

--- flash_package/p2972.sh
+++ flash_package/p2972.sh
@@ -18,38 +18,22 @@ PATH=$(pwd)/tegraflash:${PATH}
 
 TARGET_TEGRA_VERSION=t194;
 TARGET_MODULE_ID=2888;
-#TARGET_CARRIER_ID=2822;
-TARGET_CARRIER_ID=;
+TARGET_CARRIER_ID=2822;
 
 source $(pwd)/scripts/helpers.sh;
 
-declare -a FLASH_CMD_BASIC=(
-  --sdram_config tegra194-mb1-bct-memcfg-p2888.cfg,tegra194-memcfg-sw-override.cfg
-  --odmdata 0x9190000
+declare -a FLASH_CMD_EEPROM=(
   --applet mb1_t194_prod.bin
   --soft_fuses tegra194-mb1-soft-fuses-l4t.cfg
   --chip 0x19
-  --uphy_config tegra194-mb1-uphy-lane-p2888-0000-p2822-0000.cfg
-  --device_config tegra19x-mb1-bct-device-sdmmc.cfg
-  --misc_cold_boot_config tegra194-mb1-bct-misc-l4t.cfg
-  --misc_config tegra194-mb1-bct-misc-flash.cfg
-  --pinmux_config tegra19x-mb1-pinmux-p2888-0000-a04-p2822-0000-b01.cfg
-  --gpioint_config tegra194-mb1-bct-gpioint-p2888-0000-p2822-0000.cfg
-  --pmic_config tegra194-mb1-bct-pmic-p2888-0001-a04-E-0-p2822-0000.cfg
-  --pmc_config tegra19x-mb1-padvoltage-p2888-0000-a00-p2822-0000-a00.cfg
-  --prod_config tegra19x-mb1-prod-p2888-0000-p2822-0000.cfg
-  --scr_config tegra194-mb1-bct-scr-cbb-mini.cfg
-  --scr_cold_boot_config tegra194-mb1-bct-scr-cbb-mini.cfg
-  --br_cmd_config tegra194-mb1-bct-reset-p2888-0000-p2822-0000.cfg
-  --dev_params tegra194-br-bct-sdmmc.cfg
-  --bin "mb2_applet nvtboot_applet_t194.bin; mb2_bootloader nvtboot_recovery_t194.bin; mts_preboot preboot_c10_prod_cr.bin; mts_mce mce_c10_prod_cr.bin; mts_proper mts_c10_prod_cr.bin; bpmp_fw bpmp_t194.bin; bpmp_fw_dtb tegra194-a02-bpmp-p2888-a04.dtb; spe_fw spe_t194.bin; tlk tos-mon-only_t194.img; bootloader_dtb tegra194-p2888-0001-p2822-0000.dtb");
+  --bin "mb2_applet nvtboot_applet_t194.bin");
 
 if ! get_interfaces; then
   exit -1;
 fi;
 
-if ! check_module_compatibility ${TARGET_MODULE_ID}; then
-  echo "No Jetson AGX Xavier module found";
+if ! check_compatibility ${TARGET_MODULE_ID} ${TARGET_CARRIER_ID}; then
+  echo "No Jetson AGX Xavier Devkit found";
   exit -1;
 fi;
 
@@ -58,7 +42,7 @@ if ![ "${MODULEINFO[revmaj]}" == 4 -a "${MODULEINFO[revmaj]}" \> "68" ]; then
   exit -1;
 fi;
 
-declare -a FLASH_CMD_FULL=(
+declare -a FLASH_CMD_FLASH=(
   --bl nvtboot_recovery_cpu_t194.bin
   --sdram_config tegra194-mb1-bct-memcfg-p2888.cfg,tegra194-memcfg-sw-override.cfg
   --odmdata 0x9190000
@@ -80,13 +64,8 @@ declare -a FLASH_CMD_FULL=(
   --dev_params tegra194-br-bct-sdmmc.cfg
   --bin "mb2_bootloader nvtboot_recovery_t194.bin; mts_preboot preboot_c10_prod_cr.bin; mts_mce mce_c10_prod_cr.bin; mts_proper mts_c10_prod_cr.bin; bpmp_fw bpmp_t194.bin; bpmp_fw_dtb tegra194-a02-bpmp-p2888-a04.dtb; spe_fw spe_t194.bin; tlk tos-mon-only_t194.img; bootloader_dtb tegra194-p2888-0001-p2822-0000.dtb");
 
-if ! check_carrier_compatibility ${TARGET_CARRIER_ID}; then
-  echo "No Jetson AGX Xavier Devkit found";
-  exit -1;
-fi;
-
 tegraflash.py \
-  "${FLASH_CMD_FULL[@]}" \
+  "${FLASH_CMD_FLASH[@]}" \
   --instance ${INTERFACE} \
   --cfg flash_android_t194_sdmmc.xml \
   --cmd "flash; reboot"
--- flash_package/p3518.sh
+++ flash_package/p3518.sh
@@ -18,37 +18,22 @@ PATH=$(pwd)/tegraflash:${PATH}
 
 TARGET_TEGRA_VERSION=t194nx;
 TARGET_MODULE_ID=3668;
-#TARGET_CARRIER_ID=3509;
-TARGET_CARRIER_ID=;
+TARGET_CARRIER_ID=3509;
 
 source $(pwd)/scripts/helpers.sh;
 
-declare -a FLASH_CMD_BASIC=(
-  --sdram_config tegra194-mb1-bct-memcfg-p3668-0001-a00.cfg,tegra194-memcfg-sw-override.cfg
-  --odmdata 0xB8190000
+declare -a FLASH_CMD_EEPROM=(
   --applet mb1_t194_prod.bin
   --soft_fuses tegra194-mb1-soft-fuses-l4t.cfg
   --chip 0x19
-  --device_config tegra19x-mb1-bct-device-qspi-p3668.cfg
-  --misc_cold_boot_config tegra194-mb1-bct-misc-l4t.cfg
-  --misc_config tegra194-mb1-bct-misc-flash.cfg
-  --pinmux_config tegra19x-mb1-pinmux-p3668-a01.cfg
-  --gpioint_config tegra194-mb1-bct-gpioint-p3668-0001-a00.cfg
-  --pmic_config tegra194-mb1-bct-pmic-p3668-0001-a00.cfg
-  --pmc_config tegra19x-mb1-padvoltage-p3668-a01.cfg
-  --prod_config tegra19x-mb1-prod-p3668-0001-a00.cfg
-  --scr_config tegra194-mb1-bct-scr-cbb-mini-p3668.cfg
-  --scr_cold_boot_config tegra194-mb1-bct-scr-cbb-mini-p3668.cfg
-  --br_cmd_config tegra194-mb1-bct-reset-p3668-0001-a00.cfg
-  --dev_params tegra194-br-bct-qspi.cfg
-  --bin "mb2_applet nvtboot_applet_t194.bin; mb2_bootloader nvtboot_recovery_t194.bin; mts_preboot preboot_c10_prod_cr.bin; mts_mce mce_c10_prod_cr.bin; mts_proper mts_c10_prod_cr.bin; bpmp_fw bpmp_t194.bin; bpmp_fw_dtb tegra194-a02-bpmp-p3668-a00.dtb; spe_fw spe_t194.bin; tlk tos_t194.img; bootloader_dtb tegra194-p3668-all-p3509-0000-android.dtb");
+  --bin "mb2_applet nvtboot_applet_t194.bin");
 
 if ! get_interfaces; then
   exit -1;
 fi;
 
-if ! check_module_compatibility ${TARGET_MODULE_ID}; then
-  echo "No Jetson Xavier NX module found";
+if ! check_compatibility ${TARGET_MODULE_ID} ${TARGET_CARRIER_ID}; then
+  echo "No Jetson Xavier NX Devkit found";
   exit -1;
 fi;
 
@@ -57,12 +42,15 @@ NCT=;
 if [ ${MODULEINFO[sku]} -eq 1 ]; then
   FLASH_XML="flash_android_t194_spi_emmc_p3668.xml"
   NCT="p3518-0001-devkit.bin";
-else
+elif [ ${MODULEINFO[sku]} -eq 0 ]; then
   FLASH_XML="flash_android_t194_spi_sd_p3668.xml"
   NCT="p3518-0000-devkit.bin";
+else
+  echo "Unsupported Xavier NX module sku: ${MODULEINFO[sku]}";
+  exit -1;
 fi;
 
-declare -a FLASH_CMD_FULL=(
+declare -a FLASH_CMD_FLASH=(
   --bl nvtboot_recovery_cpu_t194.bin
   --sdram_config tegra194-mb1-bct-memcfg-p3668-0001-a00.cfg,tegra194-memcfg-sw-override.cfg
   --odmdata 0xB8190000
@@ -83,15 +71,10 @@ declare -a FLASH_CMD_FULL=(
   --dev_params tegra194-br-bct-qspi.cfg
   --bin "mb2_bootloader nvtboot_recovery_t194.bin; mts_preboot preboot_c10_prod_cr.bin; mts_mce mce_c10_prod_cr.bin; mts_proper mts_c10_prod_cr.bin; bpmp_fw bpmp_t194.bin; bpmp_fw_dtb tegra194-a02-bpmp-p3668-a00.dtb; spe_fw spe_t194.bin; tlk tos-mon-only_t194.img; bootloader_dtb tegra194-p3668-all-p3509-0000-android.dtb");
 
-if ! check_carrier_compatibility ${TARGET_CARRIER_ID}; then
-  echo "No Jetson Xavier NX Devkit found";
-  exit -1;
-fi;
-
 cp ${NCT} p3518.bin;
 
 tegraflash.py \
-  "${FLASH_CMD_FULL[@]}" \
+  "${FLASH_CMD_FLASH[@]}" \
   --instance ${INTERFACE} \
   --cfg ${FLASH_XML} \
   --cmd "flash; reboot"
