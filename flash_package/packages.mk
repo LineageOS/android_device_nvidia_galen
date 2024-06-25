80c4e43178bb6747b8a026e73d374aeb726a1b8c
Aaron Kling <webgeek1234@gmail.com>
2024-06-24 20:08:37 -0500

    flash_package: Update out of tree dtb path
    Using a Lineage specific repo to lessen necessary changes to upstream
    nvidia repos.
    
    Change-Id: I94f6cc8850ac00d958e0ea6f1d768d879ab1e038

--- flash_package/packages.mk
+++ flash_package/packages.mk
@@ -27,7 +27,7 @@ LPFLASH_HOST := $(HOST_OUT_EXECUTABLES)/lpflash
 ifneq ($(filter 4.9, $(TARGET_TEGRA_KERNEL)),)
 DTB_PATH := $(abspath $(KERNEL_OUT)/arch/arm64/boot/dts)
 else ifneq ($(findstring dtstree,$(TARGET_KERNEL_ADDITIONAL_FLAGS)),)
-DTB_PATH := $(abspath $(KERNEL_OUT)/../nv-oot/device-tree/platform/generic-dts/t19x/lineage)
+DTB_PATH := $(abspath $(KERNEL_OUT)/../lineage-oot/device-tree/platform/generic-dts/t19x/lineage)
 else
 DTB_PATH := $(abspath $(KERNEL_OUT)/arch/arm64/boot/dts/nvidia)
 endif
