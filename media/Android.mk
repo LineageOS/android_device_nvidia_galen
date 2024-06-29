81c5f1a8207e80d3435205c96b844dd8487cd074
Aaron Kling <webgeek1234@gmail.com>
2024-06-28 22:09:34 -0500

    Remove software omx codec references
    These are no longer available as of Android 14 QPR3. The C2 codecs
    available by default in the swcodec apex will be used instead.
    
    Change-Id: I8cdf3e700823779a7382c66c3df8410d41209b7c

--- media/Android.mk
+++ media/Android.mk
@@ -58,11 +58,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE        := media_codecs.xml
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := ETC
-ifeq ($(TARGET_TEGRA_OMX),nvmm)
 LOCAL_SRC_FILES     := media_codecs.xml
-else
-LOCAL_SRC_FILES     := media_codecs_sw.xml
-endif
 LOCAL_ODM_MODULE    := true
 include $(BUILD_PREBUILT)
 
