cd3eb5fe5ed3b8b072bc0eed023a9746031429e7
Aaron Kling <webgeek1234@gmail.com>
2025-02-13 01:00:16 -0600

    Add AGX Xavier specific perfmgr config
    These variants have eight cores in four clusters. All other supported
    variants have six cores in three clusters, as supported by the arch
    default config.
    
    Change-Id: Id233c33c3e897bbdc730b236053c6a7e2f0ad858

--- init/init_galen.cpp
+++ init/init_galen.cpp
@@ -107,4 +107,9 @@ void vendor_load_properties()
 			}
 		}
 	}
+
+	if (ti.vendor_context()) {
+		if (ti.is_model("galen"))
+			ti.property_set("vendor.powerhal.config", "powerhint.galen.json");
+	}
 }
