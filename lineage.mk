0abf57efbbc59f960d57de6a1af9b86019d25c73
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:21:39 +0100

    galen: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- lineage.mk
+++ lineage.mk
@@ -17,7 +17,7 @@ TARGET_SCREEN_HEIGHT      := 1920
 TARGET_SCREEN_WIDTH       := 1080
 
 # Unified device support
-TARGET_INIT_VENDOR_LIB := //device/nvidia/galen:libinit_galen
+$(call soong_config_set,libinit,vendor_init_lib,//device/nvidia/galen:libinit_galen)
 PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
     ro.product.vendor.device \
     ro.product.vendor.model \
